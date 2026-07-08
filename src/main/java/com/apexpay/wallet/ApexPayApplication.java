979bd5dc37b3178d99819f3f5dacb6f67315fcf8
CaedusSolo <yingtong0106@gmail.com>
2026-07-08 18:40:59 +0800

    Initial commit

---
+++ src/main/java/com/apexpay/wallet/ApexPayApplication.java
@@ -0,0 +1,13 @@
+package com.apexpay.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApexPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apexpay/wallet/ApexPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apexpay.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
